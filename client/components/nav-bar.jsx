79408c9bf75993cc41598ffa8cf4aeccb09c9102
Eugene Park <epark215@gmail.com>
2023-03-03 04:24:24 +0000

    Changed navbar color and updated favicon

--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -3,30 +3,27 @@ import { Container, Col, Nav, Navbar } from 'react-bootstrap';
 
 export default function NavBar(props) {
   return (
-    <Navbar expand="lg">
+    <Navbar>
       <Container fluid>
         <Col className='col-6'>
           <Navbar.Brand href="#home">
-            LOGO-EP
+            <img src='server/public/favicon.ico' width="32" height="32" alt='logo'/>
           </Navbar.Brand>
         </Col>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className='ms-auto' id='icon-container'>
-            <Nav.Link href="mailto: epark215@gmail.com">
-              <i className='icon fa-solid fa-envelope'/>
-            </Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/eugenepark215/">
-              <i className='icon fa-brands fa-linkedin'/>
-            </Nav.Link>
-            <Nav.Link href="https://github.com/Eugenepark215">
-              <i className='icon fa-brands fa-github' />
-            </Nav.Link>
-            <Nav.Link href="https://drive.google.com/file/d/1194BHw1F1sPn0Ne4dd5wa-MxWTPiHhu_/view?usp=share_link">
-              <i className='icon fa-solid fa-file'/>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav className='ms-auto' id='icon-container'>
+          <Nav.Link href="mailto: epark215@gmail.com">
+            <i className='icon fa-solid fa-envelope'/>
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/eugenepark215/">
+            <i className='icon fa-brands fa-linkedin'/>
+          </Nav.Link>
+          <Nav.Link href="https://github.com/Eugenepark215">
+            <i className='icon fa-brands fa-github' />
+          </Nav.Link>
+          <Nav.Link href="https://drive.google.com/file/d/1194BHw1F1sPn0Ne4dd5wa-MxWTPiHhu_/view?usp=share_link">
+            <i className='icon fa-solid fa-file'/>
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
