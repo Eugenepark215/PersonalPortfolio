1cce67240788490e73c09326baf3af2d45d7505a
Eugene Park <epark215@gmail.com>
2023-07-06 05:15:11 +0000

    stylized intro

--- client/components/intro.jsx
+++ client/components/intro.jsx
@@ -3,14 +3,13 @@ import { Row, Col } from 'react-bootstrap';
 
 export default function Intro() {
   return (
-    <Row className="justify-content-md-center intro">
-      <Col className="col-3 display-flex">
+    <Row className="justify-center intro">
+      <Col className="col-3 display-flex justify-content-md-center">
         <img src='/cat.JPG' className='intro-pic'/>
       </Col>
-      <Col className="col-6 intro-text">
-        <h2 className='gray-font'>I&#39;m Eugene!</h2>
-        <h4 className='gray-font'>A Software Developer and a fan of music!</h4>
-        <p className='gray-font'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+      <Col className="col-5 intro-text">
+        <h2 className='gray-font'>Hi There!</h2>
+        <p className='gray-font'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </Col>
     </Row>
   );
