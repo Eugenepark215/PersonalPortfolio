f7e09c728452c5f4a48eaff334fa22d1c701dce6
Eugene Park <epark215@gmail.com>
2023-07-12 19:02:57 +0000

    fade right on intropic and intro text

--- client/components/intro.jsx
+++ client/components/intro.jsx
@@ -5,9 +5,9 @@ export default function Intro() {
   return (
     <Row className="justify-center intro">
       <Col className="col-3 display-flex justify-content-md-center">
-        <img src='/cat.JPG' className='intro-pic'/>
+        <img src='/cat.JPG' className='intro-pic' data-aos="fade-right"/>
       </Col>
-      <Col className="col-5 intro-text">
+      <Col className="col-5 intro-text" data-aos="fade-right">
         <h2 className='gray-font'>Hi There!</h2>
         <p className='gray-font'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </Col>
